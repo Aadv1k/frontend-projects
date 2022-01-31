1a9843bfe3591280e4442806daddff849e5c62c1
Aadvik <aadv1k@outlook.com>
2022-01-31 23:16:32 +0530

    HTML for the lower-mid-section

--- fem-sunnyside-alp/script.js
+++ fem-sunnyside-alp/script.js
@@ -1,6 +1,6 @@
-let ham = document.getElementsByClassName('hamburger')[0];
-let navList = document.getElementsByClassName('nav__list')[0];
+let ham = document.getElementsByClassName("hamburger")[0];
+let navList = document.getElementsByClassName("nav__list")[0];
 
-ham.addEventListener('click', () => {
-    document.getElementsByClassName('nav__list')[0].classList.toggle('show')
-})
+ham.addEventListener("click", () => {
+  document.getElementsByClassName("nav__list")[0].classList.toggle("show");
+});
