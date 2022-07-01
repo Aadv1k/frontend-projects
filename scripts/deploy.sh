c9d351d4c704a5fdf68fcab92c388d97a9db4fc7
Aadv1k <aadv1k@outlook.com>
2022-07-01 21:08:57 +0530

    Add gitignore for `www/`; Refactor `deploy.sh`

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 base=$(pwd | rev | cut -d '/' -f 1 | rev)
-cp -r "./dist" $(echo "../../www/"$base)
+cp -r "./dist" "../../www/"$base
+cp "./design/desktop-preview.jpg" "../../www/"$base
 echo $base >> "../../www/filePaths.txt"
