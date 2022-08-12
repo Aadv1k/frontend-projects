aac88a5c9688449af77fffd55f7de033fea92b97
Aadv1k <aadv1k@outlook.com>
2022-08-12 13:04:29 +0530

    Add new project - @frontend-projects/uidd-product-info-ui-design-card-day-1575

---
+++ packages/uidesigndaily/uidd-product-info-ui-design-card-day-1575/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        indigo2: "#191847",
+        periwinkle: "#c9c9da",
+        blue1: "#4788e9",
+        blue2: "#1c57ee",
+        green: "#00d98b",
+      },
+
+      fontFamily: {
+        sans: ["Poppins", "system-ui"]
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
