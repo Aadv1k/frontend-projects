3a1b8c5de0276a9f9744f369335b201ac9198ae4
Aadvik <aadv1k@outlook.com>
2023-06-29 23:46:03 +0530

    updates deps and NON-WORKING config

--- packages/frontendmentor/fem-dictionary-web-app/tailwind.config.js
+++ packages/frontendmentor/fem-dictionary-web-app/tailwind.config.js
@@ -4,16 +4,7 @@ export default {
     "./index.html",
      "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-      colors: {
-        "primary": "var(--clr-primary)",
-        "secondary": "var(--clr-secondary)",
-        "headings": "var(--clr-headings)",
-        "sub-headings": "var(--clr-sub-headings)",
-        "text": "var(--clr-text)",
-        "bg":  "var(--clr-bg)"
-      }
-    },
+  theme: {},
   },
   plugins: [],
 }
