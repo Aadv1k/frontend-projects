be2dd0eb246f7c00d65808b9028d76793add6d80
aadv1k <aadv1k@outlook.com>
2023-07-26 02:41:01 +0530

    Convert the site into it's own package

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 base=$(pwd | rev | cut -d '/' -f 1 | rev)
 pwd
-cp -r "./dist" "../../../site/"$base
-cp "./design/desktop-preview.jpg" "../../../site/"$base
-# echo $base >> "../../site/filePaths.txt"
+cp -r "./dist" "../../../site/dist/"$base
+cp "./design/desktop-preview.jpg" "../../../site/dist/"$base
+# echo $base >> "../../site/dist/filePaths.txt"
--- scripts/deployStatic.sh
+++ scripts/deployStatic.sh
@@ -4,5 +4,5 @@ base=$(pwd | rev | cut -d '/' -f 1 | rev)
 
 for static in ./packages/frontendmentor/static-projects/*/ 
 do
-  cp -r "$static" ./site/
+  cp -r "$static" ./site/dist/
 done
